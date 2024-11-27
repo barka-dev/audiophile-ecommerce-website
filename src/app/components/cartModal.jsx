54f57781a26043958abf64aadc8643725c454152
Abdel <abdelbarka96@gmail.com>
2024-11-27 13:59:29 +0100

    Update cartModal.jsx

--- src/app/components/cartModal.jsx
+++ src/app/components/cartModal.jsx
@@ -9,6 +9,7 @@ export default function CartModal(){
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [show, setShow] = useState(true);
     // const [totalPrice, setTotalPrice] = useState(0);
     let totalPrice = 0;
 
@@ -44,37 +45,37 @@ export default function CartModal(){
     // if (error) return <strong>Error: {error}</strong>;
     
 
-
+    if (show)
     return(
         <>
-        <div className="overlay"></div>
-        <div className="cart-modal">
-            {
-                result.length === 0 ? <p style={{opacity:0.5, letterSpacing:2+'px'}} className="text">Empty Card</p> :
-            
-               <>
-                <div className="cart-head">
-                    <h6>cart (3)</h6>
-                    <button className="text" onClick={removeAll}>Remove all</button>
-                </div>
-                <div className="cart-body">
-                    {
-                        result.map((item, index)=>{
-                            totalPrice += item.price;
-                            return <SelectedProduct key={index} data={item}/>
-                        })
-                    }
-                </div>
-                <div className="cart-total">
-                    <p className="text">TOTAL</p>
-                    <h6>$ {totalPrice}</h6>
-                </div>
-                {/* <Link href="/checkout"> */}
-                    <button className="button orange-button cart-checkout-button">checkout</button>
-                {/* </Link> */}
-               </>     
-            }
-        </div>
+            <div className="overlay" onClick={()=>{setShow(false)}}></div>
+            <div className="cart-modal">
+                {
+                    result.length === 0 ? <p style={{opacity:0.5, letterSpacing:2+'px'}} className="text">Empty Card</p> :
+                
+                <>
+                    <div className="cart-head">
+                        <h6>cart (3)</h6>
+                        <button className="text" onClick={removeAll}>Remove all</button>
+                    </div>
+                    <div className="cart-body">
+                        {
+                            result.map((item, index)=>{
+                                totalPrice += item.price;
+                                return <SelectedProduct key={index} data={item}/>
+                            })
+                        }
+                    </div>
+                    <div className="cart-total">
+                        <p className="text">TOTAL</p>
+                        <h6>$ {totalPrice}</h6>
+                    </div>
+                    {/* <Link href="/checkout"> */}
+                        <button className="button orange-button cart-checkout-button">checkout</button>
+                    {/* </Link> */}
+                </>     
+                }
+            </div>
         </>
     )
 }
